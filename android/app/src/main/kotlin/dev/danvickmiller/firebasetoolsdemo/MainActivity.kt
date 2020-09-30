c3dc2280c4601b68c5b9f2e8714b97cc2f546df0
Danvick Miller <danvickmiller@live.com>
2020-09-30 09:18:03 +0300

    Changed app package/bundle name

--- android/app/src/main/kotlin/dev/danvickmiller/firebasetoolsdemo/MainActivity.kt
+++ android/app/src/main/kotlin/dev/danvickmiller/firebasetoolsdemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.danvickmiller.flutterstartertemplate
+package dev.danvickmiller.firebasetoolsdemo
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
